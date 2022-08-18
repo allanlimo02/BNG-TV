5b5979dcecb429a772e472f2d190a3cb79af7bc0
allanlimo02 <allanlimo02@gmail.com>
2022-08-18 21:38:27 +0300

    SplashScreen Completed

--- app/src/main/java/com/example/bngtv/Classes/SplashScreen.java
+++ app/src/main/java/com/example/bngtv/Classes/SplashScreen.java
@@ -2,15 +2,30 @@ package com.example.bngtv.Classes;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
+import com.example.bngtv.MainActivity;
 import com.example.bngtv.R;
+
 
 public class SplashScreen extends AppCompatActivity {
 
+    private static final int SPLASH_SCREEN=5000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+    }
+    private void delayer(){
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent=new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },SPLASH_SCREEN);
     }
 }
